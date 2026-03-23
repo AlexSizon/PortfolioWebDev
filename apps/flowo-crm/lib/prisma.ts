1c8baf50d5600425b19e27879aa3e6620a4d21f3
Aleksandr <a.d.sizonenko@gmail.com>
2026-03-23 13:29:55 +0200

    new prisma

--- apps/flowo-crm/lib/prisma.ts
+++ apps/flowo-crm/lib/prisma.ts
@@ -1,6 +1,6 @@
 import fs from "node:fs";
 import path from "node:path";
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
--- apps/flowo-crm/prisma/seed.ts
+++ apps/flowo-crm/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 import bcrypt from "bcryptjs";
 
